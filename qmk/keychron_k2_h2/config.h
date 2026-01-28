65b95c4e54fd6e6ee5b796d2f39bddbdee7d5508
Ben Chavez <ben.m.chavez@gmail.com>
2026-01-27 18:05:48 -0600

    updated keychon qmk

--- qmk/keychron_k2_h2/config.h
+++
@@ -1,9 +0,0 @@
-// // config.h
-
-// #pragma once
-
-// // Tap-hold timing (default is 200ms)
-// #define TAPPING_TERM 200
-
-// // Register hold even if another key is pressed and released within TAPPING_TERM
-// #define PERMISSIVE_HOLD
